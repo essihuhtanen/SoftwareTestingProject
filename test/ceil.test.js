5b98268c71be065a285e5dd881bea711dc144a2f
Miia Honkanen <miia.honkanen@tuni.fi>
2022-11-08 10:47:46 +0200

    Test CI with a failing ceil test

---
+++ test/ceil.test.js
@@ -0,0 +1,11 @@
+import chai from "chai";
+import ceil from "../src/ceil.js";
+//var chai = require("chai")
+//var ceil = require ("../ceil.js")
+const expect = chai.expect;
+
+describe("Ceil", () => {
+  it("Rounds a number to precision 1", () => {
+    expect(ceil(3.0234, 1)).to.equal(3.0);
+  });
+});
