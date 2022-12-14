78c05d7a8e9bc1277f27dcc12a9a8c3130e34bc4
Miia Honkanen <miia.honkanen@tuni.fi>
2022-12-14 12:08:55 +0200

    Add filter tests

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,3 +1,25 @@
 import chai from "chai";
 import filter from "../src/filter.js";
 const expect = chai.expect;
+
+const arr1 = [{'product' : 'cheese', 'available' : true},    
+             {'product' : 'milk', 'available' : false}];
+
+const arr2 = [{'product' : 'cheese', 'available' : false},    
+             {'product' : 'milk', 'available' : false}];
+
+const nullarr = null;
+
+describe("Filter", () => {
+    it("Function returns an array when a value meets condition", () => {
+        expect(filter(arr1, ({available}) => available )).to.be.an('array')
+    });
+    it("Function takes a null instead of array", () => {
+        expect(filter(nullarr, ({available}) => available )).to.be.an('array')
+    });
+    it("No value meets the condition", () => {
+        expect(filter(arr2, ({available}) => available )).to.be.an('array')
+    });
+  });
+
+console.log(filter(arr2, ({available}) => available ));
